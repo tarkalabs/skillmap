05ab1eca7b1032f442023ec660440dd1c18724ae
Deepak <deepak@tarkalabs.com>
2022-10-05 13:53:17 -0300

    Add OAuth2 BasicClient config for discord.

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,23 +1,60 @@
-use color_eyre::{Result, Help};
+use crate::db::DB;
+use color_eyre::{Help, Result};
+use oauth2::{basic::BasicClient, AuthUrl, ClientSecret, RedirectUrl, TokenUrl};
 use std::sync::Arc;
 use tracing::instrument;
-use crate::db::DB;
 
 #[derive(Clone, Debug)]
 pub struct AppState {
-  db: Arc<DB>,
+    db: Arc<DB>,
+    client: BasicClient,
 }
 
 impl AppState {
-  #[instrument]
-  pub async fn init() -> Result<Self> {
-    let db = DB::new().await.suggestion("Ensure that the Database URL environment variable is correct")?;
-    Ok(AppState{db: Arc::new(db)})
-  }
-  pub fn db(&self) -> Arc<DB> {
-    self.db.clone()
-  }
-  pub fn init_with_db(db: DB) -> Self {
-    AppState {db: Arc::new(db)}
-  }
+    #[instrument]
+    pub async fn init() -> Result<Self> {
+        let db = DB::new()
+            .await
+            .suggestion("Ensure that the Database URL environment variable is correct")?;
+        let client = BasicClient::new(
+            oauth2::ClientId::new("1027249500571566170".to_owned()),
+            Some(ClientSecret::new(
+                "ihmjdHIJjgWib-Z3aHzFGFMd1jpFUM8c".to_owned(),
+            )),
+            AuthUrl::new("https://discord.com/oauth2/authorize".to_owned()).unwrap(),
+            Some(TokenUrl::new("https://discord.com/api/oauth2/token".to_owned()).unwrap()),
+        )
+        .set_redirect_uri(
+            RedirectUrl::new("http://127.0.0.1:3000/auth/authorized".to_owned()).unwrap(),
+        );
+
+        Ok(AppState {
+            db: Arc::new(db),
+            client,
+        })
+    }
+    pub fn db(&self) -> Arc<DB> {
+        self.db.clone()
+    }
+    pub fn client(&self) -> BasicClient {
+        self.client.clone()
+    }
+    pub fn init_with_db(db: DB) -> Self {
+        let client = BasicClient::new(
+            oauth2::ClientId::new("1027249500571566170".to_owned()),
+            Some(ClientSecret::new(
+                "ihmjdHIJjgWib-Z3aHzFGFMd1jpFUM8c".to_owned(),
+            )),
+            AuthUrl::new("https://discord.com/oauth2/authorize".to_owned()).unwrap(),
+            Some(TokenUrl::new("https://discord.com/api/oauth2/token".to_owned()).unwrap()),
+        )
+        .set_redirect_uri(
+            RedirectUrl::new("http://127.0.0.1:3000/auth/authorized".to_owned()).unwrap(),
+        );
+
+        AppState {
+            db: Arc::new(db),
+            client,
+        }
+    }
 }
--- src/router.rs
+++ src/router.rs
@@ -1,13 +1,12 @@
-use color_eyre::Result;
 use axum::{
     body::Body,
-    Extension,
     http::{Request, StatusCode},
-    Json,
-    response::{Response, IntoResponse},
-    Router, 
-    routing::{get, post}
+    response::{IntoResponse, Response, Redirect},
+    routing::{get, post},
+    Extension, Json, Router,
 };
+use color_eyre::Result;
+use oauth2::{CsrfToken, Scope};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 use tracing::instrument;
@@ -20,41 +19,55 @@ async fn home_handler() -> String {
 
 #[instrument]
 async fn users_handler(req: Request<Body>) -> Result<Json<Vec<User>>, StatusCode> {
-    let state = req.extensions().get::<AppState>().ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
-    let users = User::all(&state.db().connection())
-        .await
-        .map_err(|err| {
-            tracing::error!("{:?}", err);
-            StatusCode::INTERNAL_SERVER_ERROR
-        })?;
+    let state = req
+        .extensions()
+        .get::<AppState>()
+        .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+    let users = User::all(&state.db().connection()).await.map_err(|err| {
+        tracing::error!("{:?}", err);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
     Ok(Json(users))
 }
 
 #[instrument]
-async fn create_user_handler(Json(mut payload): Json<User>, Extension(state): Extension<AppState>) -> Result<Response, StatusCode> {
-    payload.insert(&state.db().connection())
+async fn create_user_handler(
+    Json(mut payload): Json<User>,
+    Extension(state): Extension<AppState>,
+) -> Result<Response, StatusCode> {
+    payload
+        .insert(&state.db().connection())
         .await
         .map_err(|err| {
             tracing::error!("{:?}", err);
             StatusCode::INTERNAL_SERVER_ERROR
         })?;
-    Ok((
-        StatusCode::CREATED,
-        [("Content-Type", "application/json")]
-    ).into_response())
+    Ok((StatusCode::CREATED, [("Content-Type", "application/json")]).into_response())
+}
+
+#[instrument]
+async fn discord_auth(Extension(state): Extension<AppState>) -> impl IntoResponse {
+    let (auth_url, _csrf_token) = &state.client()
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("identify".to_string()))
+        .url();
+
+    // Redirect to Discord's oauth service
+    Redirect::to(auth_url.as_ref())
 }
 
 #[instrument]
 pub async fn build_router(app_state: AppState) -> Result<Router<Body>> {
     // let shared_state = app_state::AppState::init().await.context("error initializing state")?;
     let router = Router::new()
-    .route("/", get(home_handler))
-    .route("/users", get(users_handler))
-    .route("/users", post(create_user_handler))
-    .layer(
-        ServiceBuilder::new()
-            .layer(Extension(app_state))
-            .layer(TraceLayer::new_for_http())
-    );
+        .route("/", get(home_handler))
+        .route("/users", get(users_handler))
+        .route("/users", post(create_user_handler))
+        .route("/auth/discord", get(discord_auth))
+        .layer(
+            ServiceBuilder::new()
+                .layer(Extension(app_state))
+                .layer(TraceLayer::new_for_http()),
+        );
     Ok(router)
 }
